83adae422200d1b09f01a9ca382fe94963cdfbb8
First Last <kevin622@yonsei.ac.kr>
2022-02-20 23:09:17 -0800

    working on how the model works

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import argparse
 import random
 
 import gym
+from gym.wrappers import FrameStack, ResizeObservation
 import torch
 import numpy as np
 
 from replay_buffer import ReplayBuffer
-from models import DeterministicWorldModel, Policy
+from models import WorldModel, Policy
 
 
 def main():
@@ -22,14 +23,17 @@ def main():
     args = parser.parse_args()
 
     env = gym.make(args.env_name)
+    env = ResizeObservation(env, (105, 80))
+    env = FrameStack(env, 4)
+
     env.seed(args.seed)
     random.seed(args.seed)
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
     policy = Policy()
-    world_model = DeterministicWorldModel(pixel_shape=env.observation_space[0],
-                                          action_shape=env.action_space.n)
+    world_model = WorldModel(in_channel_size=env.observation_space[0] * 4,
+                                          action_size=env.action_space.n)
     replay_buffer = ReplayBuffer(args.buffer_size)
 
     pass
--- models.py
+++ models.py
@@ -5,10 +5,11 @@ import torch.nn.functional as F
 # World Model
 # NN simulated env
 # train to mimic real world env
-class DeterministicWorldModel(nn.Module):
-    def __init__(self, pixel_shape, action_shape):
+class WorldModel(nn.Module):
+    def __init__(self, in_channel_size, action_size):
         super().__init__()
-        self.input_frame_dense = nn.Linear(pixel_shape, 96)
+        # Frame Prediction
+        self.input_frame_dense = nn.Linear(in_channel_size, 96)
         self.downscale_conv1 = nn.Conv2d(96, 192, kernel_size=(4, 4), stride=(2, 2))
         self.downscale_conv2 = nn.Conv2d(192, 384, kernel_size=(4, 4), stride=(2, 2))
         self.downscale_conv3 = nn.Conv2d(384, 768, kernel_size=(4, 4), stride=(2, 2))
@@ -16,8 +17,28 @@ class DeterministicWorldModel(nn.Module):
         self.downscale_conv5 = nn.Conv2d(768, 768, kernel_size=(4, 4), stride=(2, 2))
         self.downscale_conv6 = nn.Conv2d(768, 768, kernel_size=(4, 4), stride=(2, 2))
 
-        self.action_embedding = nn.Embedding(action_shape, 768)
-        
+        self.action_embedding = nn.Linear(action_size, 768)
+        self.latent_pred_embedding = nn.Embedding()
+
+        self.middle_conv1 = nn.Conv2d(768, 768, kernel_size=(3, 3), stride=(1, 1))
+        self.middle_conv2 = nn.Conv2d(768, 768, kernel_size=(3, 3), stride=(1, 1))
+
+        self.upscale_trans_conv1 = nn.ConvTranspose2d(768, 768, kernel_size=(4, 4), stride=(2, 2))
+        self.upscale_trans_conv2 = nn.ConvTranspose2d(768, 768, kernel_size=(4, 4), stride=(2, 2))
+        self.upscale_trans_conv3 = nn.ConvTranspose2d(768, 768, kernel_size=(4, 4), stride=(2, 2))
+        self.upscale_trans_conv4 = nn.ConvTranspose2d(768, 384, kernel_size=(4, 4), stride=(2, 2))
+        self.upscale_trans_conv5 = nn.ConvTranspose2d(384, 192, kernel_size=(4, 4), stride=(2, 2))
+        self.upscale_trans_conv5 = nn.ConvTranspose2d(192, 96, kernel_size=(4, 4), stride=(2, 2))
+
+        self.output_frame_dense = nn.Linear(96, 768)
+
+        self.dropout = nn.Dropout(p=0.2)
+        self.relu = nn.ReLU()
+
+    
+    def forward(self, input_frame, action, target_frame=None):
+        pass
+
 
 
 
